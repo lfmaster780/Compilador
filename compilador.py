9259541fe84d4fb0d8c11d0a5d6d966f4fd16296
lfmaster780 <lfilipe780@gmail.com>
2019-10-06 21:39:49 -0300

    compilador sintatico e lexico prontos

---
+++ compilador.py
@@ -0,0 +1,10 @@
+from Lexico.lexer import Token
+from Lexico.lexer import Scanner
+from Sintatico.parser import Parser
+
+print("FOI")
+ent = input()
+lexer = Scanner(ent)
+resultado = lexer.scanTokens()
+parser = Parser()
+parser.iniciar(resultado)
